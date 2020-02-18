a0a31d400f23d4b34e96517da49d3fa088dacafa
gnardinosaur <github email address>
2020-02-18 16:11:15 -0500

    updated user stock list logic so that it updates quicker

--- frontend/src/components/UserStocks.js
+++ frontend/src/components/UserStocks.js
@@ -3,11 +3,11 @@ import { List, Container, Grid, Header } from 'semantic-ui-react';
 import { formatter } from '../constants/formatCurrency';
 
 class UserStocks extends React.Component {
-  _isMounted = false;
-
+  
   state = {
     userStocks: [],
-    summedArray: []
+    summedArray: [],
+    updateStockList: false
   }
 
   componentDidMount(){
@@ -17,19 +17,17 @@ class UserStocks extends React.Component {
   };
 
   //update user's list of stocks when new stocks are purchased 
-  componentDidUpdate(prevProps, prevState) { 
-    if(this.state !== prevState) {
-      if(this._isMounted) {
+  componentDidUpdate(prevProps) { 
+    if(this.props !== prevProps) {
+      if(this.props.updateStockList) {
         fetch(`http://localhost:3000/api/v1/users/${this.props.user.id}/transactions`)
         .then(resp => resp.json())
-        .then(userStocks => this.setState({ userStocks }, () => this.buildUserStockObject())) 
+        .then(userStocks => this.setState({ 
+          userStocks,
+          updateStockList: false
+        }, () => this.buildUserStockObject())) 
       }
     }
-  }
-
-  //fn to make sure component doesn't continue to try check state --> this prevents memory leak error and increases preformance 
-  componentWillUnmount() {
-    this._isMounted = false;
   }
 
   buildUserStockObject = () => {
