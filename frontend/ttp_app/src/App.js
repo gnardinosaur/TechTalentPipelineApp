738d7048d0482bc90fd22b754b63b249087ee1af
gnardinosaur <github email address>
2020-02-18 10:12:17 -0500

    portfolio page showing owned share * current price

--- frontend/ttp_app/src/App.js
+++ frontend/ttp_app/src/App.js
@@ -5,6 +5,7 @@ import Register from './components/Register';
 import SignIn from './components/SignIn';
 import Transactions from './components/Transactions';
 import Portfolio from './components/Portfolio';
+import { convertStringToNum } from './constants/formatCurrency';
 
 class App extends React.Component {
 
@@ -20,12 +21,23 @@ class App extends React.Component {
     this.setState({ user })
   }
 
+  decreaseCash = (totalPurchasePrice) => {
+    const newBalance = parseInt(this.state.user.cash) - convertStringToNum(totalPurchasePrice)
+    debugger;
+    this.setState({ 
+      user: {
+        ...this.state.user,
+        cash: newBalance
+      }
+    }, () => console.log(this.state))
+  }
+
   render(){
     return (
       <div className='App'>
         <Switch>
           <Route path='/sign_in' render={(routerProps) => <SignIn {...routerProps} user={this.state.user} setUser={this.setUser} />} />
-          <Route path='/portfolio' render={(routerProps)  => <Portfolio user={this.state.user} {...routerProps} />} />
+          <Route path='/portfolio' render={(routerProps)  => <Portfolio user={this.state.user} {...routerProps} decreaseCash={this.decreaseCash} />} />
           <Route path='/transactions' render={(routerProps) => <Transactions {...routerProps} />} />
           <Route path='/' render={(routerProps) => <Register {...routerProps} user={this.state.user} setUser={this.setUser} />} />
         </Switch>
